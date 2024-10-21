4f6dccb67085d772d0ff2704285b91e03426af65
Somadina <somadinao@icloud.com>
2024-10-21 18:14:46 +0100

    Security features

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const rateLimit = require("express-rate-limit");
 const globalErrorHandler = require("./controllers/errorController");
 const AppError = require("./util/appError");
 const userRouter = require("./routes/userRoutes");
@@ -9,6 +15,23 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use(helmet());
+
+const limiter = rateLimit({
+  max: 500,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this ip address, try again in 1hour.",
+});
+app.use("/api", limiter);
+
+// Data sanitization against NOSQL query injection
+app.use(mongoSanitize());
+// Data sanitization against XSS
+app.use(xss());
+// Prevent parameter pollution
+app.use(hpp());
+app.use(compression());
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/expenses", expenseRouter);
